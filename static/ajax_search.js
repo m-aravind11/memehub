9ee38a4dfc0bbd02b92e06c374805cf225056964
m-aravind11 <maravind2000@gmail.com>
2021-01-15 20:38:54 +0530

    Fixed bug on clear search results

--- static/ajax_search.js
+++ static/ajax_search.js
@@ -11,6 +11,7 @@ $("button#search_button").on("click", function(){
 
           $.each(response.result, function(index, template){
             console.log(template.template_filename);
+            $("div#searched_query_content").empty();
             $("div#searched_query_content").append(' <a href=' + template.onClickURL + '> <img class="p-3" src='+ template.onClickURL +' width=360px height=270px/> </a> </div>');
           });
         }
