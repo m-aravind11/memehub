7da94f311fa99ca542ff87cc2c909cb5b3a377f7
m-aravind11 <maravind2000@gmail.com>
2021-01-16 21:46:59 +0530

    quotes bug fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import dropbox
 
 dropbox_access_token='gFooMCn0-lMAAAAAAAAAAcrMLIMb6ygl1B3pUrVjoWD8_-jGjU67Hzbf3HZJ633r'
-'
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iobztsjapjlhkq:10af9d6965ef13dbca3b4aa2195f50788f0db49a1add8fb772a4d84181dc6104@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d3fgd14rl7d0tk'
 app.config['SECRET_KEY'] = 'secret'
