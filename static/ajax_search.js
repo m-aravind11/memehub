b6b13a74d6bc0eaaad300dde39fa89517d71afae
m-aravind11 <maravind2000@gmail.com>
2021-01-17 22:12:49 +0530

    image not display bug fix

--- static/ajax_search.js
+++ static/ajax_search.js
@@ -10,8 +10,8 @@ $("button#search_button").on("click", function(){
           $("div#searched_query_content").empty();
 
           $.each(response.result, function(index, template){
-            var htmlcard='<div class="col-lg-4 m-2 template-card-search border border-secondary rounded"> \
-              <img class="template-card-search-image m-1 text-center" src=' +  template.onClickURL + 'alt="Card image"/> \
+            var htmlcard='<div class="col-lg-3 m-2 template-card-search border border-secondary rounded"> \
+              <img class="template-card-search-image m-1 text-center" src="' +  template.onClickURL + '"alt="Card image"/> \
               <div class="card-body"> \
                 <h4 class="card-title">' + template.dialogue + '</h4> \
                 <div class="card-text"> <label class="font-weight-bold" for="movie_template_name">' + "Movie/Template Name:" + '</label> ' + template.movie_name + '</div> \
